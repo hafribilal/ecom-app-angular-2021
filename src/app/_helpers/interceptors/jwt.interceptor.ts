54166d3fb7cf8585eba9d31b16e0e745adf74087
Bilal Hafri <hafribilal@gmail.com>
2021-05-30 18:58:16 +0100

    setup jwt interceptor to add the Authorization token if available

---
+++ src/app/_helpers/interceptors/jwt.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+	HttpRequest,
+	HttpHandler,
+	HttpEvent,
+	HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class JWTInterceptor implements HttpInterceptor {
+
+	constructor() { }
+
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		// add authorization header with jwt token if available
+		let token = localStorage.getItem(environment.JWT);
+		if (token) {
+			request = request.clone({
+				setHeaders: {
+					Authorization: `Bearer ${token}`
+				}
+			});
+		}
+
+		return next.handle(request);
+	}
+}
