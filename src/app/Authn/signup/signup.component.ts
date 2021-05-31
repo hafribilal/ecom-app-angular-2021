c8d31405823f5d1a68b00ece91d1d1273eca2533
Bilal Hafri <hafribilal@gmail.com>
2021-05-31 02:15:10 +0100

    some odification in signup form interface and setup the submit with the auth service

--- src/app/Authn/signup/signup.component.ts
+++ src/app/Authn/signup/signup.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Client } from 'src/app/models/client/client.module';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+	selector: 'app-signup',
+	templateUrl: './signup.component.html',
+	styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+	client!: Client;
+	confirm_password!: string;
+	terms: boolean = false;
 
-  ngOnInit(): void {
-  }
+	constructor(private auth: AuthService) {
+		this.client = new Client()
+	}
+
+	ngOnInit(): void {
+		// this.client.prenom = "test";
+		// this.client.nom = "test";
+		// this.client.username = "test10";
+		// this.client.email = this.client.username + "@example.com";
+		// this.client.password = "testtest";
+		// this.confirm_password = "testtest";
+		// this.client.ville = "testCity";
+		// this.client.tele = "0987654321";
+		// this.terms = true;
+	}
+
+	async signup() {
+		if (this.client.password === this.confirm_password && this.terms) {
+			this.client = await this.auth.clientSignUp(this.client);
+			setTimeout(() => { console.log(this.client) }, 2500);
+		}
+
+	}
 
 }
