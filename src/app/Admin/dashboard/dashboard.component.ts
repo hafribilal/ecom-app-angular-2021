66d6dafa435b07c9feffe39748f0d485d848f4a8
Bilal Hafri <hafribilal@gmail.com>
2021-06-02 20:43:54 +0100

    setup dashboard to recieve articles and also setup create-product to add products and update it

--- src/app/Admin/dashboard/dashboard.component.ts
+++ src/app/Admin/dashboard/dashboard.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { ArticleModule } from 'src/app/models/article/article.module';
 
-
+const baseURL = "/articles";
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
+	articles!: ArticleModule[];
+	images!: Array<string>;
+	constructor(private api: ApiService) { }
 
-  
-  constructor() {} 
+	ngOnInit(): void {
+		this.getArticles();
+	}
 
-  ngOnInit(): void {
-    
-  }
+	async getArticles() {
+		this.articles = await this.api.getAll(baseURL + "/my");
+		console.log(this.articles)
+	}
 
 
 }
--- src/app/Admin/product-create/product-create.component.ts
+++ src/app/Admin/product-create/product-create.component.ts
@@ -1,39 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ArticleModule } from 'src/app/models/article/article.module';
+import { ApiService } from 'src/app/services/api.service';
 
-
+const baseURL = "/articles";
 @Component({
-  selector: 'product-create',
-  templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.css']
+	selector: 'product-create',
+	templateUrl: './product-create.component.html',
+	styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
 
-  
+	article!: ArticleModule;
+	constructor(private api: ApiService) { }
 
-  constructor() { }
+	ngOnInit(): void {
+		this.article = new ArticleModule();
+	}
 
-  ngOnInit(): void {
-  }
 
+	form = new FormGroup({
+		productRef: new FormControl({ value: '00', disabled: true }, [Validators.required]),
+		prodName: new FormControl('', [Validators.required]),
+		prodPrice: new FormControl('', [Validators.required]),
+		description: new FormControl('', [Validators.required])
+	});
 
-  form = new FormGroup({
-    productRef: new FormControl('', [Validators.required]),
-    prodName: new FormControl('', [Validators.required]),
-    prodPrice: new FormControl('', [Validators.required]),
-    description: new FormControl('', [Validators.required])
-    });
+	get f() {
+		return this.form.controls;
+	}
 
-    get f() {
-      return this.form.controls;
-    }
-     
-    submit() {
+	submit() {
+		console.log("SUBMIT")
+		if (this.form.status === 'VALID') {
+			if (this.form.value['productRef'] != "00") {
+				this.article.id = this.form.value['productRef'];
+			}
+			this.article.titre = this.form.value['prodName'];
+			this.article.prix = this.form.value['prodPrice'];
+			this.article.description = this.form.value['description'];
+			console.log(this.article);
+			if (this.form.value['productRef'] != "00") {
+				console.log("create");
+				this.create();
+			} else {
+				console.log("update");
+				this.update();
+			}
+		}
+	}
 
-      if (this.form.status === 'VALID') {
-         console.log(this.form.value);
-      }
-
-    }
-
+	create() {
+		this.api.add(baseURL + "/create", this.article);
+	}
+	update() {
+		this.api.update(baseURL + "/update", this.article);
+	}
 }
