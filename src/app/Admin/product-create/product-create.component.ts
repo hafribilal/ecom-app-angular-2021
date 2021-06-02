9409668fd398156a09d42467e0782c118ffca01c
Bilal Hafri <hafribilal@gmail.com>
2021-06-02 23:40:55 +0100

    update create product component

--- src/app/Admin/product-create/product-create.component.ts
+++ src/app/Admin/product-create/product-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ArticleModule } from 'src/app/models/article/article.module';
 import { ApiService } from 'src/app/services/api.service';
@@ -10,7 +10,8 @@ const baseURL = "/articles";
 	styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
-
+	@Input()
+	@Output()
 	article!: ArticleModule;
 	constructor(private api: ApiService) { }
 
@@ -23,6 +24,7 @@ export class ProductCreateComponent implements OnInit {
 		productRef: new FormControl({ value: '00', disabled: true }, [Validators.required]),
 		prodName: new FormControl('', [Validators.required]),
 		prodPrice: new FormControl('', [Validators.required]),
+		prodStock: new FormControl('', [Validators.required]),
 		description: new FormControl('', [Validators.required])
 	});
 
@@ -38,8 +40,8 @@ export class ProductCreateComponent implements OnInit {
 			}
 			this.article.titre = this.form.value['prodName'];
 			this.article.prix = this.form.value['prodPrice'];
+			this.article.stock = this.form.value['prodStock'];
 			this.article.description = this.form.value['description'];
-			console.log(this.article);
 			if (this.form.value['productRef'] != "00") {
 				console.log("create");
 				this.create();
