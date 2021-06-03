d283ba1fa20df6fdfb85283f2c521884cd9d21c8
Bilal Hafri <hafribilal@gmail.com>
2021-06-03 17:17:17 +0100

    remove unusable imports

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Compt } from '../models/compt/compt.module';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { IApi } from './i-api';
-import { ArticleModule } from '../models/article/article.module';
 
 @Injectable({
 	providedIn: 'root'
