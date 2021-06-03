a52afe9610d9e4ad3b260aa48790a278f88f0072
Bilal Hafri <hafribilal@gmail.com>
2021-06-03 18:41:34 +0100

    this service is to sync the components between each others

---
+++ src/app/services/common.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { ArticleModule } from '../models/article/article.module';
+import { Subject, Observer } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class CommonService {
+
+	panier = new Subject<number>();
+	articles = new Subject<ArticleModule[]>();
+	telecast = this.articles.asObservable();
+	constructor(private api: ApiService) { }
+
+	updatePanier() {
+		this.api.getOne('/panier/count').then(
+			(count) => {
+				this.panier.next(count);
+			}
+		).catch(
+			(err) => {
+				console.log(err.status);
+			}
+		);
+	}
+
+	updateArticles() {
+		this.api.getOne('/articles/my').then(
+			(articles) => {
+				this.articles.next(articles);
+			}
+		).catch(
+			(err) => {
+				console.log(err.status);
+			}
+		);
+	}
+
+	getArticles() {
+		return this.articles.asObservable();
+	}
+
+	getPanier() {
+		return this.panier.asObservable();
+	}
+}
