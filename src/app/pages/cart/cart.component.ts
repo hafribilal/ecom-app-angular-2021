9ef9ff359f66d38a20cb28dc46e217707d408532
Bilal Hafri <hafribilal@gmail.com>
2021-06-03 22:24:26 +0100

    setup cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { PanierModule } from 'src/app/models/panier/panier.module';
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+	selector: 'app-cart',
+	templateUrl: './cart.component.html',
+	styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+	paniers!: Array<PanierModule>;
+	constructor(private api: ApiService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.paniers = new Array();
+		this.api.getAll("/panier/all").then(
+			(paniers) => {
+				this.paniers = paniers;
+				console.log(paniers)
+			}
+		);
+	}
+
+	load() {
+
+	}
+
+	delete(id: number) {
+		this.api.delete("/panier/" + id).then(
+			() => {
+				this.paniers = this.paniers.filter(obj => obj.id != id);
+			}
+		).catch(
+			(err) => {
+				console.log(err.message)
+			}
+		);
+	}
 
 }
