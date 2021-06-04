83cf27b02f7de416d57a3143bf10a7aeef820d25
Bilal Hafri <hafribilal@gmail.com>
2021-06-04 05:10:07 +0100

    config some route paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,21 +10,24 @@ import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListarticlesComponent } from './pages/listarticles/listarticles.component';
+import { AuthGuard } from './_helpers/guards/auth.guard';
+import { ClientGuard } from './_helpers/guards/client.guard';
+import { AdminGuard } from './_helpers/guards/admin.guard';
 
 const routes: Routes = [
 
 	{ path: '', redirectTo: '/home', pathMatch: 'full' },
 	{ path: 'home', component: HomeComponent },
-	{ path: 'login', component: LoginComponent/*, outlet : "contenu"*/ },
-	{ path: 'signup', component: SignupComponent/*, outlet : "contenu"*/ },
-	{ path: 'shop', component: ListarticlesComponent/*, outlet : "autre"*/ },
-	{ path: 'cart', component: CartComponent },
-	{ path: 'changepassword', component: ChangepasswordComponent },
-	{ path: 'checkout', component: CheckoutComponent },
-	{ path: 'productdetail/:id', component: ArticledetailComponent, pathMatch: 'full' },
-	{ path: 'dashboard', component: DashboardComponent },
-	{ path: 'AdminSignUp', component: SignUpComponent }
-
+	{ path: 'login', component: LoginComponent },
+	{ path: 'client/signup', component: SignupComponent },
+	{ path: 'admin/signup', component: SignUpComponent },
+	{ path: 'changepassword', component: ChangepasswordComponent, canActivate: [AuthGuard] },
+	{ path: 'dashboard', component: DashboardComponent, canActivate: [AdminGuard] },
+	{ path: 'shop', component: ListarticlesComponent, canActivate: [AuthGuard] },
+	{ path: 'productdetail/:id', component: ArticledetailComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+	{ path: 'cart', component: CartComponent, canActivate: [ClientGuard] },
+	{ path: 'checkout', component: CheckoutComponent, canActivate: [ClientGuard] },
+	{ path: '**', component: HomeComponent }
 ];
 
 @NgModule({
