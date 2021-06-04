b15d5873487a1d1fd8bc0baa1b91a0f1ae0e301e
Bilal Hafri <hafribilal@gmail.com>
2021-06-04 05:12:36 +0100

    add current user role update in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	constructor(private common: CommonService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		if (localStorage.getItem("USER_ROLE") === "USER") {
+			this.common.updatePanier();
+		}
+	}
 
 }
